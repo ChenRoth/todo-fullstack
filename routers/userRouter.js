771eaa82fabbaf38124424e1f11f5b62da42503b
Chen Roth <rothchen@gmail.com>
2020-04-07 19:26:09 +0300

    add user router

---
+++ routers/userRouter.js
@@ -0,0 +1,11 @@
+const {Router} = require('express');
+
+const userRouter = Router();
+
+// userRouter.post('/login', (req, res) => {
+
+// });
+
+module.exports = {
+    userRouter
+}
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 const expressJwt = require('express-jwt');
 
 const {todoRouter} = require('./routers/todoRouter');
+const {userRouter} = require('./routers/userRouter');
+
 
 const app = express();
 const PORT = 3001;
@@ -11,6 +13,7 @@ const SECRET = 'chocolate-cake';
 
 app.use(expressJwt({secret: SECRET}).unless({path: []})); 
 app.use(express.json());
+app.use('/users', userRouter);
 app.use('/todos', todoRouter);
 
 app.listen(PORT, () => {
