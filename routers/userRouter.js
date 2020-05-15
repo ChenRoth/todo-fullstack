2e519bcf16b280e60edf7ba14e6a7f59006c3a51
Chen Roth <rothchen@gmail.com>
2020-05-15 12:05:30 +0300

    add /register endpoint

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,8 +1,25 @@
 const { Router } = require('express');
 const jwt = require('jsonwebtoken');
-const { readUsers } = require('../db');
+const { readUsers, writeUsers } = require('../db');
 
 const userRouter = Router();
+
+userRouter.post('/register', async (req, res) => {
+    const { id, password } = req.body;
+    const users = await readUsers();
+    if (users[id]) {
+        res.status(400).send({ success: false, msg: 'user already exists' });
+        return;
+    }
+
+    users[id] = { id, password };
+    await writeUsers(users);
+
+    const token = generateToken(users[id]);
+
+    res.send({ success: true, msg: 'welcome!', token });
+});
+
 
 userRouter.post('/login', async (req, res) => {
     const { id, password } = req.body;
